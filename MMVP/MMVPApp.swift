321e7971fab4cf7431c757b0a27564e4af7d6fc7
Sem C <semcruiming@gmail.com>
2024-08-01 11:35:15 +0200

    Initial Commit

---
+++ MMVP/MMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMVPApp.swift
+//  MMVP
+//
+//  Created by Sem Cruiming on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
